8e4f49d812ad7897bede097cdffa06e089782bea
alexuhui <610927985@qq.com>
2022-11-24 14:23:27 +0800

    删除依赖库

--- Project/OpenGLHelloWorld/OpenGLHelloWorld/main.cpp
+++ Project/OpenGLHelloWorld/OpenGLHelloWorld/main.cpp
@@ -2,13 +2,56 @@
 #include <GLFW\glfw3.h>
 #include <iostream>
 
+#define numVAOS 1
+
 using namespace std;
 
-void init(GLFWwindow* window) { }
+
+GLuint renderingProgram;
+GLuint vao[numVAOS];
+
+GLuint createShaderProgram()
+{
+    const char *vshaderSource =
+        "#version 430 \n"
+        "void main(void) \n"
+        "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+
+    const char* fshaderSource =
+        "#version 430 \n"
+        "out vec4 color; \n"
+        "void main(void) \n"
+        "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1, &fshaderSource, NULL);
+
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
+}
+
+void init(GLFWwindow* window) { 
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOS, vao);
+    glBindVertexArray(vao[0]);
+}
 
 void display(GLFWwindow* window, double currentTime) {
-    glClearColor(0.5, 0.5, 0.5, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    //glClearColor(0.5, 0.5, 0.5, 1.0);
+    //glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(renderingProgram);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main(void) {
